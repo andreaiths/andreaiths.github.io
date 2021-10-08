ac596146f1a45311a632616d226e04f973a638c8
Andrea Öberg <74138953+andreaoberg@users.noreply.github.com>
2021-10-08 18:18:24 +0200

    Create quizQuestions.js

---
+++ react-multi-choice-quiz-master/src/api/quizQuestions.js
@@ -0,0 +1,219 @@
+var quizQuestions = [
+  {
+      question: "Hur många mellannamn har du?",
+      answers: [
+          {
+              type: "GIN & TONIC ",
+              content: "0"
+          },
+          {
+              type: "GIN/APPLE & TONIC ",
+              content: "1"
+          },
+          {
+              type: "GIN BITTER LEMON",
+              content: "2+"
+          }
+      ]
+  },
+  {
+      question: "Hur många ex har du?",
+      answers: [
+        {
+            type: "CITRUS FIZZ",
+            content: "0"
+        },
+          {
+              type: "REDBULL VODKA",
+              content: "1"
+          },
+          {
+              type: "GIN & TONIC",
+              content: "2"
+          },
+          {
+              type: "GIN BITTER LEMON",
+              content: "3+"
+          }
+      ]
+  },
+  {
+      question: "Favoritegenskap hos framtida partner?",
+      answers: [
+          {
+              type: "CITRUS FIZZ",
+              content: "Charmig"
+          },
+          {
+              type: "GIN/APPLE TONIC",
+              content: "Rolig"
+          },
+          {
+            type: "GIN & TONIC",
+            content: "Lugn"
+          },
+          {
+            type: "REDBULL VODKA",
+            content: "Energisk"
+          }
+      ]
+  },
+  {
+      question: "Favortfärg på klocka?",
+      answers: [
+          {
+              type: "GIN BITTER LEMON",
+              content: "Rosé"
+          },
+          {
+              type: "GIN & TONIC",
+              content: "Guld"
+          },
+          {
+              type: "CITRUS FIZZ",
+              content: "Silver"
+          },
+          {
+              type: "GIN/APPLE TONIC",
+              content: "Grön"
+          }
+      ]
+  },
+  {
+      question: "Vilket stjärntecken är du?",
+      answers: [
+          {
+              type: "REDBULL VODKA",
+              content: "Vattuman"
+          },
+          {
+            type: "GIN & TONIC",
+            content: "Fisk"
+        },
+        {
+            type: "GIN/APPLE TONIC",
+            content: "Vädur"
+        },
+        {
+            type: "GIN BITTER LEMON",
+            content: "Oxe"
+        },
+        {
+            type: "CITRUS FIZZ",
+            content: "Tvilling"
+        },
+        {
+            type: "GIN & TONIC",
+            content: "Kräfta"
+        },
+        {
+            type: "GIN/APPLE TONIC",
+            content: "Lejon"
+        },
+          {
+              type: "GIN BITTER LEMON",
+              content: "Jungfru"
+          },
+          {
+            type: "REDBULL VODKA",
+            content: "Våg"
+        },
+        {
+            type: "CITRUS FIZZ",
+            content: "Skorpion"
+        },
+        {
+            type: "GIN & TONIC",
+            content: "Skytt"
+        },
+          {
+              type: "GIN/APPLE TONIC",
+              content: "Stenbock"
+          }
+      ]
+  },
+  {
+    question: "Hur gammal är du?",
+    answers: [
+        {
+            type: "GIN BITTER LEMON",
+            content: "16-23"
+        },
+        {
+            type: "REDBULL VODKA",
+            content: "24-29"
+        },
+        {
+            type: "GIN/APPLE TONIC",
+            content: "30+"
+        }
+    ]
+}, {
+    question: "Vem gillar du mest?",
+    answers: [
+        {
+            type: "GIN & TONIC",
+            content: "Lisa"
+        },
+        {
+            type: "GIN BITTER LEMON",
+            content: "Alexandra"
+        },
+        {
+            type: "REDBULL VODKA",
+            content: "Praktikanterna"
+        }
+    ]
+}, {
+    question: "Vad har du för klack på dina skor just nu?",
+    answers: [
+        {
+            type: "GIN/APPLE TONIC",
+            content: "Ingen klack"
+        },
+        {
+            type: "GIN BITTER LEMON",
+            content: "Låg-medel klack"
+        },
+        {
+            type: "CITRUS FIZZ",
+            content: "Jag-kommer-bryta-benen klack"
+        }
+    ]
+}, {
+    question: "Hur många kuddar har du i din säng?",
+    answers: [
+        {
+            type: "CITRUS FIZZ",
+            content: "0-2"
+        },
+        {
+            type: "GIN & TONIC",
+            content: "3-4"
+        },
+        {
+            type: "GIN/APPLE TONIC",
+            content: "4+"
+        }
+    ]
+}, {
+    question: "Vad föredrar du?",
+    answers: [
+        {
+            type: "GIN/APPLE TONIC",
+            content: "Sött"
+        },
+        {
+            type: "REDBULL VODKA",
+            content: "Salt"
+        },
+        {
+            type: "CITRUS FIZZ",
+            content: "Surt"
+        }
+    ]
+}
+
+];
+
+export default quizQuestions;
